f002908ba47c914eae0aaed557ee40ead9dcb249
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-05 13:59:59 -0300

    feat: create styled ButtonModal component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./sidebar.module.scss";
 import SearchBar from "./components/SearchBar";
 import Links from "./components/Links";
+import ButtonModal from "./components/ButtonModal";
 
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <SearchBar />
       <Links />
+      <ButtonModal />
     </aside>
   );
 }
