9e3fbd62cc6e81cbc85bad2c14d0109d82c6e963
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-05 16:23:36 -0300

    feat: create styled Disciplines component

--- src/View/App.tsx
+++ src/View/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Disciplines from "./Disciplines";
+
+import styles from "./app.module.scss";
 
 function App() {
   return (
-    <div>
+    <div className={styles.app}>
       <Sidebar />
+      <Disciplines />
     </div>
   );
 }
