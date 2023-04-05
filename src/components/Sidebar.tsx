6eaee9a830c8928b32c5fbeec87612f62e12adc7
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-05 11:32:50 -0300

    feat: create HTML structure for sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>1º Período</li>
+          <li>2º Período</li>
+          <li>3º Período</li>
+          <li>4º Período</li>
+          <li>5º Período</li>
+          <li>6º Período</li>
+          <li>7º Período</li>
+          <li>8º Período</li>
+          <li>Optativas</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
