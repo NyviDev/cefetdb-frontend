932167007b3fcc7bbfd2d0b2e35b0f4ec04366ca
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-05 13:31:40 -0300

    feat: create Links component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import styles from "./sidebar.module.scss";
 import SearchBar from "./components/SearchBar";
+import Links from "./components/Links";
 
 function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <SearchBar />
-      <nav>
-        <ul>
-          <li className={styles.item}>1º Período</li>
-          <li className={styles.item}>2º Período</li>
-          <li className={`${styles.item} ${styles.active}`}>3º Período</li>
-          <li className={styles.item}>4º Período</li>
-          <li className={styles.item}>5º Período</li>
-          <li className={styles.item}>6º Período</li>
-          <li className={styles.item}>7º Período</li>
-          <li className={styles.item}>8º Período</li>
-          <li className={styles.item}>Optativas</li>
-        </ul>
-      </nav>
+      <Links />
     </aside>
   );
 }
