38541f199b5ddcd226cb58d5040ba1391c553697
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-06 10:30:14 -0300

    create: create styled Footer component

--- src/View/App.tsx
+++ src/View/App.tsx
@@ -3,13 +3,17 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import Disciplines from "./Disciplines";
 
 import styles from "./app.module.scss";
+import Footer from "../components/Footer/Footer";
 
 function App() {
   return (
+    <>
     <div className={styles.app}>
       <Sidebar />
       <Disciplines />
     </div>
+    <Footer/>
+    </>
   );
 }
 
