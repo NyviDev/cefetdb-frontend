e539d6865c39911dd4805c41e6bd34dc816552c5
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-10 13:27:34 -0300

    feat: separate component into smaller ones for better organization

--- src/View/Home/index.tsx
+++ src/View/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Home.module.scss";
-import Disciplines from "../../components/Disciplines";
+import Disciplines from "../../components/Disciplines/Disciplines";
 
 const Home = () => {
   
