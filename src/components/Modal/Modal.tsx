be9935c0d5fa90a13a72c890aa1ee40b218530cb
Nicolly Vieira <nicollyvscosta@gmail.com>
2023-04-13 11:33:28 -0300

    style(modal): improve modal styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect, useState } from "react";
 import { ModalContext } from "../../contexts/ModalContext";
 import { AiOutlineClose } from "react-icons/ai";
 
@@ -6,6 +6,23 @@ import styles from "./modal.module.scss";
 
 const Modal = () => {
   const modal = useContext(ModalContext);
+  const ref = useRef<HTMLInputElement>(null);
+  const [haveFile, setHaveFile] = useState(false);
+  const [nameFile, setNameFile] = useState("");
+
+  useEffect(() => {
+    if (ref.current) {
+      setHaveFile(false);
+      ref.current.addEventListener("change", () => {
+        let directoryFile = ref.current?.value;
+        let nameFileArray = directoryFile?.split("C:\\fakepath\\");
+        if (nameFileArray) {
+          setNameFile(nameFileArray[1]);
+        }
+        setHaveFile(true);
+      });
+    }
+  }, [modal.modalIsOpen]);
 
   return (
     <>
@@ -17,17 +34,24 @@ const Modal = () => {
             </button>
             <h3 className={styles.title}>Envie uma prova</h3>
             <form className={styles.form}>
-              <select name="courses" id="courses">
-                <option value="course1">
-                  Gestão do Conhecimento da Informação
-                </option>
-                <option value="course2">Cálculo</option>
-                <option value="course3">Programação para Clientes Web</option>
-              </select>
-              <label htmlFor="file" className={styles.input}>
-                Enviar um arquivo
-                <input id="file" type="file" />
-              </label>
+              <div className={styles.formWrapper}>
+                <select name="courses" id="courses">
+                  <option value="course1">
+                    Gestão do Conhecimento da Informação
+                  </option>
+                  <option value="course2">Cálculo</option>
+                  <option value="course3">Programação para Clientes Web</option>
+                </select>
+                <label htmlFor="file" className={styles.input}>
+                  Enviar um arquivo
+                  <input ref={ref} id="file" type="file" />
+                </label>
+              </div>
+              {haveFile ? (
+                <p className={styles.fileFound}>{nameFile}</p>
+              ) : (
+                <p className={styles.notFound}>Nenhum Arquivo Encontrado</p>
+              )}
             </form>
           </div>
         </div>
